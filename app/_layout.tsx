d3bfce45755e3494ec165dd353f4a2617e861e7f
mapokapo <leopetrovic11@gmail.com>
2025-01-07 17:13:20 +0100

    fix: add toaster

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,10 +3,12 @@ import { Slot } from "expo-router";
 
 import "../global.css";
 import { UserProvider } from "@/lib/context/user-provider";
+import { Toaster } from "burnt/web";
 
 export default function RootLayout() {
   return (
     <UserProvider>
+      <Toaster />
       <Slot />
     </UserProvider>
   );
