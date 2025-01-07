6eab45480208f950576b9bd107714da7e3079c61
mapokapo <leopetrovic11@gmail.com>
2025-01-07 21:59:11 +0100

    refactor: improve input component and add icon support

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -4,11 +4,11 @@ import { Text, TextInput, TextInputProps, View } from "react-native";
 import { tv, type VariantProps } from "tailwind-variants";
 
 const inputVariants = tv({
-  base: "h-10 px-4 py-2 rounded-md border border-input",
+  base: "h-10 px-4 py-2 flex-1 rounded-md",
   variants: {
     variant: {
       default: "bg-background text-foreground",
-      error: "border border-destructive bg-background text-destructive",
+      error: "bg-background text-destructive",
     },
   },
   defaultVariants: {
@@ -24,6 +24,7 @@ export interface InputProps
   errorText?: string;
   labelClassName?: string;
   label?: string;
+  leftIcon?: React.ReactNode;
 }
 
 const Input: React.FC<InputProps> = ({
@@ -34,6 +35,7 @@ const Input: React.FC<InputProps> = ({
   variant,
   label,
   errorText,
+  leftIcon,
   ...props
 }) => {
   return (
@@ -43,16 +45,23 @@ const Input: React.FC<InputProps> = ({
           {label}
         </Text>
       )}
-      <TextInput
+      <View
         className={cn(
-          inputVariants({
-            variant,
-            className: textInputClassName,
-          })
-        )}
-        placeholderClassName="text-muted-foreground"
-        {...props}
-      />
+          "flex-row items-center rounded-md border border-input",
+          errorText && "border-destructive"
+        )}>
+        <View className="pl-2">{leftIcon}</View>
+        <TextInput
+          className={cn(
+            inputVariants({
+              variant,
+              className: textInputClassName,
+            })
+          )}
+          placeholderClassName="text-muted-foreground"
+          {...props}
+        />
+      </View>
       {errorText && (
         <Text className={cn("text-sm text-destructive", errorTextClassName)}>
           {errorText}
