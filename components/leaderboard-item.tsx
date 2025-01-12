7f1c97f04cc9aac14535a740a85d9b4ab1c03feb
mapokapo <leopetrovic11@gmail.com>
2025-01-12 16:08:09 +0100

    refactor: change LeaderboardItem props

--- components/leaderboard-item.tsx
+++ components/leaderboard-item.tsx
@@ -1,11 +1,10 @@
-import { User } from "@/lib/context/user-provider";
 import { Tables } from "@/lib/types/SupabaseDatabaseTypes";
 import { cn } from "@/lib/utils";
 import { Text, View } from "react-native";
 
 type Props = {
   profile: Tables<"profiles">;
-  currentUser: User;
+  currentUser: boolean;
   index: number;
 };
 
@@ -13,7 +12,7 @@ const LeaderboardItem: React.FC<Props> = ({ profile, currentUser, index }) => (
   <View
     className={cn(
       "flex-row justify-between border-b border-gray-200 px-4 py-2",
-      profile.id === currentUser.id && "bg-gray-100"
+      currentUser && "bg-gray-100"
     )}>
     <Text>
       {index + 1}. {profile.name}
