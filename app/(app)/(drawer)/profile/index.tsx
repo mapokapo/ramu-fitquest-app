84d68852187e1c66260a469c176c200cb6cda637
mapokapo <leopetrovic11@gmail.com>
2025-01-21 13:21:43 +0100

    refactor: move challenges translation map to new file

--- app/(app)/(drawer)/profile/index.tsx
+++ app/(app)/(drawer)/profile/index.tsx
@@ -9,32 +9,7 @@ import { useAppUser } from "@/lib/context/user-provider";
 import { useEffect, useState } from "react";
 import { Text, View, Image } from "react-native";
 import { Link } from "expo-router";
-
-const challengesTranslationMap = (
-  challengeCode: string,
-  value: number
-): string | null => {
-  const map: Record<string, ((value: number) => string) | undefined> = {
-    walk_steps: (steps: number) => `Pređite ${steps} koraka`,
-    do_pushups: (pushups: number) => `Napravite ${pushups} sklekova`,
-    stretch_mins: (mins: number) => `Odradite ${mins} minuta istezanja`,
-    drink_water_mls: (mls: number) => `Popijte ${mls} ml vode`,
-    plank_mins: (mins: number) => `Odradite plank ${mins} minuta`,
-    read_pages: (pages: number) => `Pročitajte ${pages} stranica knjige`,
-    do_situps: (situps: number) => `Napravite ${situps} trbušnjaka`,
-    do_squats: (squats: number) => `Napravite ${squats} čučnjeva`,
-    walk_kms: (kms: number) => `Pređite ${kms} kilometara`,
-    do_yoga_mins: (mins: number) => `Odradite ${mins} minuta joge`,
-  };
-
-  const translation = map[challengeCode];
-
-  if (!translation) {
-    return null;
-  }
-
-  return translation(value);
-};
+import { challengesTranslationMap } from "@/lib/const/challenges-translation-map";
 
 export default function Profile() {
   const user = useAppUser();
